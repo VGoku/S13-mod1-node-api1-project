fd2b9ec3136b3b8cd080e6ccc80e75a84fd48493
David S. <neojohn2312@gmail.com>
2024-09-27 15:59:08 -0500

    2nd-commit

--- api/server.js
+++ api/server.js
@@ -1,11 +1,44 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
-
+const User = require("./users/model")
 const server = express();
+
+server.get("/api/users", (req, res) => {
+    // console.log("getting all users");
+     // res.json("users")
+    User.find()
+   .then(users => {
+    // throw new Error("Argh!!!!!!!!!")
+    // console.log(users);
+    res.json(users)
+   })
+   .catch(err => {
+    res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+        stack: err.stack,
+    })
+   })
+})
+
+server.get("/api/users/:id", (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        // console.log(user)
+        res.json(user)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "error getting users",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+})
 
 server.use("*", (req, res) => {
     res.status(404).json({
-        message: "Not Found!!!"
+        message: "Not Found!!"
     })
 })
 
