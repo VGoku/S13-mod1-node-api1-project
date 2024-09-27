88e15f262e0bdba63a69be54af53bef74e2207cf
David S. <neojohn2312@gmail.com>
2024-09-27 18:24:19 -0500

    4th-commit

--- api/server.js
+++ api/server.js
@@ -1,7 +1,37 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
 const User = require("./users/model")
+
 const server = express();
+server.use(express.json())
+
+// Root route
+// server.get("/", (req, res) => {
+//     res.send("Welcome to the API!"); // You can customize this message
+// });
+
+server.post("/api/users", (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+           
+            res.status(201).json(createdUser)
+              console.log(createdUser);
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error creating users",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
 
 server.get("/api/users", (req, res) => {
     // console.log("getting all users");
