c37a09cee0565d091f90e6d11ad8ef599c312638
David S. <neojohn2312@gmail.com>
2024-09-27 16:06:25 -0500

    3rd-commit

--- api/server.js
+++ api/server.js
@@ -25,11 +25,15 @@ server.get("/api/users/:id", (req, res) => {
     User.findById(req.params.id)
     .then(user => {
         // console.log(user)
+        if (!user) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist"})
+        }
         res.json(user)
     })
     .catch(err => {
         res.status(500).json({
-            message: "error getting users",
+            message: "error getting user",
             err: err.message,
             stack: err.stack,
         })
