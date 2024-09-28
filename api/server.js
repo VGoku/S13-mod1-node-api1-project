11d246f4e5b37d954376f11ebc6c180585eaa22b
David S. <neojohn2312@gmail.com>
2024-09-27 20:21:19 -0500

    7th-commit

--- api/server.js
+++ api/server.js
@@ -6,7 +6,8 @@ const server = express();
 server.use(express.json())
 
 server.delete("/api/users/:id", async (req, res) => {
-const possibleUser = await User.findById(req.params.id);
+try {
+    const possibleUser = await User.findById(req.params.id);
 // console.log("posssible user", possibleUser)
 if (!possibleUser) {
     res.status(404).json({
@@ -16,6 +17,13 @@ if (!possibleUser) {
     const deletedUser = await User.remove(possibleUser.id)
     res.status(200).json(deletedUser)
     // console.log(stuff);
+} 
+} catch (err) {
+    res.status(500).json({
+        message: "error creating user",
+        err: err.message,
+        stack: err.stack,
+    })
 }
 })
 
